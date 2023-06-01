703141c5e82dab77f2f65b04458858753c317c38
KevinSpigel <kevinspigel@gmail.com>
2023-06-02 00:22:49 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import FirstMiddleware from './middleware/firstMiddleware';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URL'),
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(FirstMiddleware).forRoutes('*');
+  }
+}
